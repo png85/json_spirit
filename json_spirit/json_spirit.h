a062a5e979a0ccefaaf0791bf52999298bd11399
Peter Hille (png!das-system) <peter@das-system-networks.de>
2013-04-18 18:20:58 +0200

    bump version number to json_spirit 4.06

--- json_spirit/json_spirit.h
+++ json_spirit/json_spirit.h
@@ -4,7 +4,7 @@
 //          Copyright John W. Wilkinson 2007 - 2011
 // Distributed under the MIT License, see accompanying file LICENSE.txt
 
-// json spirit version 4.05
+// json spirit version 4.06
 
 #if defined(_MSC_VER) && (_MSC_VER >= 1020)
 # pragma once
