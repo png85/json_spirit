d4e9e846d8ae6790ec253cc2f3c2c11fa6243259
moony <sm.lee4573@hd-bsnc.com>
2019-01-21 11:58:18 +0900

    can be built using vcpkg on windows

--- json_spirit/json_spirit_writer.h
+++ json_spirit/json_spirit_writer.h
@@ -11,6 +11,7 @@
 # pragma once
 #endif
 
+#include "json_spirit_global.h"
 #include "json_spirit_value.h"
 #include "json_spirit_writer_options.h"
 #include <iostream>
@@ -20,44 +21,44 @@ namespace json_spirit
     // these functions to convert JSON Values to text
 
 #ifdef JSON_SPIRIT_VALUE_ENABLED
-    void         write( const Value&  value, std::ostream&  os, unsigned int options = 0 );
-    std::string  write( const Value&  value, unsigned int options = 0 );
+    json_spirit_EXPORT void         write( const Value&  value, std::ostream&  os, unsigned int options = 0 );
+    json_spirit_EXPORT std::string  write( const Value&  value, unsigned int options = 0 );
 #endif
 
 #ifdef JSON_SPIRIT_MVALUE_ENABLED
-    void         write( const mValue& value, std::ostream&  os, unsigned int options = 0 );
-    std::string  write( const mValue& value, unsigned int options = 0 );
+    json_spirit_EXPORT void         write( const mValue& value, std::ostream&  os, unsigned int options = 0 );
+    json_spirit_EXPORT std::string  write( const mValue& value, unsigned int options = 0 );
 #endif
 
 #if defined( JSON_SPIRIT_WVALUE_ENABLED ) && !defined( BOOST_NO_STD_WSTRING )
-    void         write( const wValue&  value, std::wostream& os, unsigned int options = 0 );
-    std::wstring write( const wValue&  value, unsigned int options = 0 );
+    json_spirit_EXPORT void         write( const wValue&  value, std::wostream& os, unsigned int options = 0 );
+    json_spirit_EXPORT std::wstring write( const wValue&  value, unsigned int options = 0 );
 #endif
 
 #if defined( JSON_SPIRIT_WMVALUE_ENABLED ) && !defined( BOOST_NO_STD_WSTRING )
-    void         write( const wmValue& value, std::wostream& os, unsigned int options = 0 );
-    std::wstring write( const wmValue& value, unsigned int options = 0 );
+    json_spirit_EXPORT void         write( const wmValue& value, std::wostream& os, unsigned int options = 0 );
+    json_spirit_EXPORT std::wstring write( const wmValue& value, unsigned int options = 0 );
 #endif
 
     // these "formatted" versions of the "write" functions are the equivalent of the above functions
     // with option "pretty_print"
     
 #ifdef JSON_SPIRIT_VALUE_ENABLED
-    void         write_formatted( const Value& value, std::ostream&  os );
-    std::string  write_formatted( const Value& value );
+    json_spirit_EXPORT void         write_formatted( const Value& value, std::ostream&  os );
+    json_spirit_EXPORT std::string  write_formatted( const Value& value );
 #endif
 #ifdef JSON_SPIRIT_MVALUE_ENABLED
-    void         write_formatted( const mValue& value, std::ostream&  os );
-    std::string  write_formatted( const mValue& value );
+    json_spirit_EXPORT void         write_formatted( const mValue& value, std::ostream&  os );
+    json_spirit_EXPORT std::string  write_formatted( const mValue& value );
 #endif
 
 #if defined( JSON_SPIRIT_WVALUE_ENABLED ) && !defined( BOOST_NO_STD_WSTRING )
-    void         write_formatted( const wValue& value, std::wostream& os );
-    std::wstring write_formatted( const wValue& value );
+    json_spirit_EXPORT void         write_formatted( const wValue& value, std::wostream& os );
+    json_spirit_EXPORT std::wstring write_formatted( const wValue& value );
 #endif
 #if defined( JSON_SPIRIT_WMVALUE_ENABLED ) && !defined( BOOST_NO_STD_WSTRING )
-    void         write_formatted( const wmValue& value, std::wostream& os );
-    std::wstring write_formatted( const wmValue& value );
+    json_spirit_EXPORT void         write_formatted( const wmValue& value, std::wostream& os );
+    json_spirit_EXPORT std::wstring write_formatted( const wmValue& value );
 #endif
 }
 
